cdfe14be8c681b1e712b920691c09077fa2b90a3
VoTrieuAn <voan2068@gmail.com>
2023-11-06 21:01:16 +0700

    Create project

---
+++ dangnhap.js
@@ -0,0 +1,64 @@
+const form = document.querySelector('#create-account-form');
+const usernameInput = document.querySelector('#username');
+const titleInput = document.querySelector('#title');
+
+form.addEventListener('submit', (event)=>{
+    
+    validateForm();
+    console.log(isFormValid());
+    if(isFormValid()==true){
+        form.submit();
+     }else {
+         event.preventDefault();
+     }
+
+});
+
+function isFormValid(){
+    const inputContainers = form.querySelectorAll('.input-group');
+    let result = true;
+    inputContainers.forEach((container)=>{
+        if(container.classList.contains('error')){
+            result = false;
+        }
+    });
+    return result;
+} 
+
+function validateForm() {
+    //USERNAME
+    if(usernameInput.value.trim()==''){
+        setError(usernameInput, 'Điền đầy đủ họ và tên');
+    }else if(usernameInput.value.trim()=='Đặng Nguyễn Cẩm Tiên' || usernameInput.value.trim()=='Đặng Nguyễn Cẩm Tiên'){
+        setSuccess(usernameInput);
+    }else {
+        setError(usernameInput, 'Điền chuẩn đi làm ơnnnn');
+    }
+    //TITLE
+    if(titleInput.value.trim()==''){
+        setError(titleInput, 'Phải điền cả cái này nữa ');
+    }else if(titleInput.value.trim()=='03102004' || titleInput.value.trim()=='3102004') {
+        setSuccess(titleInput);
+    }else {
+        setError(titleInput, 'Sai mật khẩu')    
+    }
+
+}
+
+function setError(element, errorMessage) {
+    const parent = element.parentElement;
+    if(parent.classList.contains('success')){
+        parent.classList.remove('success');
+    }
+    parent.classList.add('error');
+    const paragraph = parent.querySelector('p');
+    paragraph.textContent = errorMessage;
+}
+
+function setSuccess(element){
+    const parent = element.parentElement;
+    if(parent.classList.contains('error')){
+        parent.classList.remove('error');
+    }
+    parent.classList.add('success');
+}
